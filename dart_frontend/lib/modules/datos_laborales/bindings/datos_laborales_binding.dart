13c541c57a99bde5b7bc49be8301be5df4e36bbe
Roger <rogerzapatista@gmail.com>
2025-07-14 14:14:21 -0600

    .

--- dart_frontend/lib/modules/datos_laborales/bindings/datos_laborales_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/datos_laborales_controller.dart';
-
-class DatosLaboralesBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<DatosLaboralesController>(() => DatosLaboralesController());
-  }
-}
--- dart_frontend/lib/modules/datos_personales/bindings/datos_personales_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/datos_personales_controller.dart';
-
-class DatosPersonalesBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<DatosPersonalesController>(() => DatosPersonalesController());
-  }
-}
